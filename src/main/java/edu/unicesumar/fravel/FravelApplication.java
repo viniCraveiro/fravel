c0502803dbde4653f702713e4d4f63a78b6753e3
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2024-07-14 19:52:06 -0300

    first commit

---
+++ src/main/java/edu/unicesumar/fravel/FravelApplication.java
@@ -0,0 +1,13 @@
+package edu.unicesumar.fravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unicesumar/fravel/FravelApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unicesumar.fravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
